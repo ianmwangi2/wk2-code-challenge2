39d1352ead95e587324b3ed678fddbf1739999c4
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-08-11 21:56:10 +0300

    Added the data of events to be displayed as cards

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function BotCard() {
-  return (
-    <div>BotCard</div>
-  )
-}
-
-export default BotCard
+    return (
+        <div className="bot-card">
+          <img src={bot.avatar_url} alt={bot.name} />
+          <h3>NAME: {bot.name}</h3>
+          <p>Class: {bot.bot_class}</p>
+          <button onClick={() => showDetails(bot)}>View Details</button>
+          <button onClick={() => handleEnlist(bot)}>Enlist</button>
+          <button onClick={() => handleDelete(bot.id)}>X</button>
+        </div>
+      );
+    }
+    
+    export default BotCard;
