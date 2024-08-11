fa5c317d777be8d4200dd799c020e74f97bb20f2
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-08-11 22:31:41 +0300

    solved error

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import BotCard from './BotCard';
 
 function BotCollection({ handleEnlist, handleDelete, showDetails, filteredBots }) {
-    const [bots, setBots] = useState([]);
-  
-    useEffect(() => {
-      fetch('http://localhost:3000/bots')
-        .then((resp) => resp.json())
-        .then((data) => setBots(data));
-    }, []);
-  
-    const displayedBots = filteredBots(bots);
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/bots')
+      .then((resp) => resp.json())
+      .then((data) => setBots(data));
+  }, []);
+
+  const displayedBots = filteredBots(bots);
 
   return (
-    <div className='bot-collection'>
+    <div className="bot-collection">
       {displayedBots.length ? (
         displayedBots.map((bot) => (
           <BotCard
@@ -25,10 +25,10 @@ function BotCollection({ handleEnlist, handleDelete, showDetails, filteredBots }
           />
         ))
       ) : (
-        <p>No bots found</p>
+        <p>BOTS</p>
       )}
     </div>
-  )
+  );
 }
 
-export default BotCollection
+export default BotCollection;
