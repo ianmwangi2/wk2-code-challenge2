d7a4423183e35c721522fde2cfa817246295eb26
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-08-11 22:27:10 +0300

    created variablles and imported useSTate

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import BotSpecs from './components/BotSpecs';
 import SortBar from './components/SortBar';
 import FilterBar from './components/FilterBar';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [army, setArmy] = useState([]);
+  const [selectedBot, setSelectedBot] = useState(null);
+  const [sortCriteria, setSortCriteria] = useState(null);
+  const [filters, setFilters] = useState([]);
+
   return (
-    <div>
-      <h1>
-        WELCOME TO BATTLR
-      </h1>
+    <div className="App">
+      {selectedBot ? (
+        <BotSpecs
+          bot={selectedBot}
+          handleEnlist={enlistBot}
+          handleBack={() => setSelectedBot(null)}
+        />
+      ) : (
+        <>
+          <h1>WELCOME TO BOT BATTLR</h1>
+          <SortBar handleSort={handleSort} />
+          <FilterBar handleFilter={handleFilter} />
+          <BotCollection
+            handleEnlist={enlistBot}
+            handleDelete={deleteBot}
+            showDetails={setSelectedBot}
+            filteredBots={filteredBots}
+          />
+          <YourBotArmy bots={army} handleRelease={releaseBot} />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
